2600a222be2ab743b2bf0d19430e0676f6e639a4
YeshiLhazom <92807458+YeshiLhazom@users.noreply.github.com>
2022-02-15 08:49:14 +0600

    Layout is added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 
 import {Button} from 'react-bootstrap'
 import { Entry } from './page/entry/Entry page';
 
 
 function App() {
-  return <div className="App">
-  <Entry />
-  </div>;
+  return(
+    <div className="App">
+  {/*<Entry />*/}
+  <DefaultLayout>
+    //Dashboard
+  </DefaultLayout>
+  </div>
+  );
 }
 
 export default App;
---
+++ src/layout/partials/Footer.comp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return(
+  <div className="text-center copy-right">
+    &copy; Attendance Management System all right reserved -2022.
+  </div>
+  ); 
+};
